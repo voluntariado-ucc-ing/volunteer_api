4903cfd75b1ce3895fdb0881d17b2ce0dc0172fb
lmurature <lucio.murature@mercadolibre.com>
2020-09-14 23:32:21 -0300

    refactor mail body

--- providers/mail_provider.go
+++ providers/mail_provider.go
@@ -18,7 +18,7 @@ func SendMail(emailAddress string, password string) {
 	msg := []byte(fmt.Sprintf("To: %s\r\n" +
 		"Subject: ¡Fuiste aceptado en Voluntariado Ingeniería UCC!\r\n" +
 		"\r\n" +
-		"¡Enhorabuena voluntario! \n Tu acceso al sistema del Voluntariado UCC fue aprobado, tu clave es %s . Apenas ingreses, podrás cambiar tu contraseña por la que desees. \n Muchas gracias.\r\n", emailAddress, password))
+		"¡Enhorabuena voluntario!\n\n\nTu acceso al sistema del Voluntariado UCC fue aprobado, tu clave es %s .\nApenas ingreses, podrás cambiar tu contraseña por la que desees.\nMuchas gracias.\r\n", emailAddress, password))
 	err := smtp.SendMail(
 		config.SmtpAddress,
 		auth,
